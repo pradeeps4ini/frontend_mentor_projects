5a3b0a7133f8a8184b1b72fad0e64a0487955777
pradeeps4ini <pradeepsaini@protonmail.com>
2022-08-02 13:32:47 +0530

    user can sent message to the chat

---
+++ chat_app/js/main.js
@@ -0,0 +1,64 @@
+const domElements = (() => {
+  const messageInputBox = document.getElementById("input-message");
+  const messageSubmit = document.getElementById("send-chat");
+  const chatMessages = document.getElementById("chat-box__messages");
+
+  return { messageInputBox, messageSubmit, chatMessages };
+})();
+
+
+const randomNumber = function() {
+
+  const rand = Math.floor(Math.random() * 6);
+  return rand;
+};
+
+
+const publishUserChat = function(message, chatMessages) {
+  const userChatDiv = document.createElement("div");
+  const p = document.createElement("p");
+  p.textContent = message;
+
+  userChatDiv.append(p);
+  userChatDiv.classList.add("chat__user");
+
+  chatMessages.appendChild(userChatDiv);
+};
+
+
+const freelancerChat = function(chats, chatMessages) {
+  const randIndex = randomNumber();
+  const chat = chats[randIndex];
+ 
+  const div = document.createElement("div");
+  const p = document.createElement("p");
+  p.textContent = chat;
+  div.classList.add("chat__walker");
+  div.appendChild(p);
+  chatMessages.appendChild(div);
+};
+
+
+const domInteraction = (() => {
+
+  const { messageInputBox, messageSubmit, chatMessages } = { ...domElements };
+  
+  const autoChat = ["Your dogs are cute.", 
+                    "Lets do this.",
+                    "I am ready to walk them.",
+                    "I'll pick up in evening.",
+                    "I am good.",
+                    "They are great."];
+
+  messageSubmit.addEventListener("click", (e) => {
+    e.preventDefault;
+    const message = messageInputBox.value;
+    messageInputBox.value = "";
+    
+    publishUserChat(message, chatMessages);
+   
+    setTimeout(() => {
+      freelancerChat(autoChat, chatMessages);
+    }, 1000)
+  });
+})();
